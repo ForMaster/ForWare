a8542ae8b927ea53c2716b31431c1e5f9b39db40
ForMaster <105603409+ForMaster@users.noreply.github.com>
2022-05-15 21:06:06 +0300

    Add files via upload

---
+++ Forware_082709.py
@@ -0,0 +1,114 @@
+while True:
+    print( "                     By ForMaster" )
+    print( "                     My SYSTEM FORWARE OF PYTHON" )
+    print( "https://www.youtube.com/channel/UCHSJ9V_1oQATLmD4YMuhLcA" )
+    print( "Welcom to FORWARE!" )
+    print( "Enter off in consol to deactivate ForWare")
+    print( "" )
+    print( "" )
+    print( "Files, System, Memory, ARM." )
+    disk = input( "C/:user>" )
+    if disk == "Files":
+        print( "Videos_and_photos(Vap), Apps, " )
+        change_Files = input( "C/:user>Files/" )
+        if change_Files == "Vap":
+            print( "this folder is empty..." )
+        else:
+            print( "" )
+        if change_Files == "off":
+            break
+        if change_Files == "Apps":
+            print( "Youtube, Recorder, Among_Us, Minecraft." )
+        else:
+            print( "" )
+    else:
+        print( "" )
+    if disk == "off":
+        break
+    if disk == "System":
+        print( "Bios, System_32, Camera_drivers, Video_card_drivers." )
+        change_System = input( "C:/user>System/" )
+        if change_System == "Bios":
+            print( "Boot_system, Deactivate_system, Power_unit." )
+        else:
+            print( "" )
+            if change_System == "off":
+                break
+        if change_System == "System_32":
+            print( "Start_bios, Files_system_32, Programm_files." )
+        else:
+            print( "" )
+            if change_System == "camera_drivers":
+                print( "You can't open this files!" )
+            else:
+                print( "" )
+            if change_System == "Video_card_drivers":
+                print( "You can't open this files!" )
+            else:
+                print( "" )
+        print( "" )
+        if disk == "Memory":
+            print( "Empty 5/10 gb." )
+        else:
+            print( "" )
+            print( "Boot_System, Deactivate_system, Power_unit.")
+            if disk == "Files_Bios":
+                print( "ARM5, FORWARE Kit, As." )
+        change_System = input( "C:/user>System>Bios>Boot_System>Files_Bios/" )
+        if change_System == "ARM5":
+            print( "Files_Bios_ARM5, Afor, Python Code FORWARE." )
+        else:
+            print( "" )
+            if change_System == "off":
+                break
+        if change_System == "Afor":
+            print( "ForMaster, codebioARM, Programm." )
+        else:
+            print( "" )
+            if change_System == "ForMaster":
+                print( "ARM 5 NO FORMASTER!" )
+            else:
+                print( "" )
+            if change_System == "Files_Bios_ARM5":
+                print( "Bios ARM 1.0.2!" )
+                if disk == "off":
+                    break
+    if disk == "ARM":
+        print( "ForMaster, Ms WARE, drivers ARM, KILL." )
+        change_ARM = input( "C:/user>ARM/" )
+        if change_ARM == "DARM":
+            print( "hig, Deactivate_ARM, ARM1." )
+        else:
+            print( "" )
+            if change_ARM == "off":
+                break
+        if change_ARM == "IIIARM":
+            print( "ARM_bios, File_ARM, Programm_ARM." )
+        else:
+            print( "" )
+            if change_ARM == "File_ARM":
+                print( "DIR S CD C SS!" )
+            else:
+                print( "" )
+            if change_ARM == "ARM_bios":
+                print( "bios s s dir s ARM!" )
+    if disk == "Ms WARE Pocket":
+        print( "Instal Folder Ms WARE, developer kit Ms WARE, SIK TO KICK, SOFT." )
+        change_ARM = input( "C:/user>ARM>Ms WARE>Ms WARE Pocket/" )
+        if change_ARM == "SOFT":
+            print( "Defult MS, custompip, Ms TWO." )
+        else:
+            print( "" )
+            if change_ARM == "off":
+                break
+        if change_ARM == "Ms TWO":
+            print( "Y, N." )
+        else:
+            print( "" )
+            if change_ARM == "Y":
+                print( "you have successfully installed ms two!" )
+            else:
+                print( "" )
+            if change_ARM == "N":
+                print( "at the request of the administrator , the pocket ms two did not settle down!" )
+
---
+++ Forware_114717.py
@@ -0,0 +1,137 @@
+while True:
+    print( "                     By ForMaster" )
+    print( "                     My SYSTEM FORWARE OF PYTHON" )
+    print( "https://www.youtube.com/channel/UCHSJ9V_1oQATLmD4YMuhLcA" )
+    print( "Welcom to FORWARE!" )
+    print( "Enter off in consol to deactivate ForWare")
+    print( "" )
+    print( "" )
+    print( "Files, System, Memory, ARM, Help, Help in Russia." )
+    disk = input( "C/:user>" )
+    if disk == "Files":
+        print( "Videos_and_photos(Vap), Apps, " )
+        change_Files = input( "C/:user>Files/" )
+        if change_Files == "Vap":
+            print( "this folder is empty..." )
+        else:
+            print( "" )
+        if change_Files == "off":
+            break
+        if change_Files == "Apps":
+            print( "Youtube, Recorder, Among_Us, Minecraft." )
+        else:
+            print( "" )
+    else:
+        print( "" )
+    if disk == "off":
+        break
+    if disk == "System":
+        print( "Bios, System_32, Camera_drivers, Video_card_drivers." )
+        change_System = input( "C:/user>System/" )
+        if change_System == "Bios":
+            print( "Boot_system, Deactivate_system, Power_unit." )
+        else:
+            print( "" )
+            if change_System == "off":
+                break
+        if change_System == "System_32":
+            print( "Start_bios, Files_system_32, Programm_files." )
+        else:
+            print( "" )
+            if change_System == "camera_drivers":
+                print( "You can't open this files!" )
+            else:
+                print( "" )
+            if change_System == "Video_card_drivers":
+                print( "You can't open this files!" )
+            else:
+                print( "" )
+        print( "" )
+        if disk == "Memory":
+            print( "Empty 5/10 gb." )
+        else:
+            print( "" )
+            print( "Boot_System, Deactivate_system, Power_unit.")
+            if disk == "Files_Bios":
+                print( "ARM5, FORWARE Kit, As." )
+        change_System = input( "C:/user>System>Bios>Boot_System>Files_Bios/" )
+        if change_System == "ARM5":
+            print( "Files_Bios_ARM5, Afor, Python Code FORWARE." )
+        else:
+            print( "" )
+            if change_System == "off":
+                break
+        if change_System == "Afor":
+            print( "ForMaster, codebioARM, Programm." )
+        else:
+            print( "" )
+            if change_System == "ForMaster":
+                print( "ARM 5 NO FORMASTER!" )
+            else:
+                print( "" )
+            if change_System == "Files_Bios_ARM5":
+                print( "Bios ARM 1.0.2!" )
+                if disk == "off":
+                    break
+    if disk == "ARM":
+        print( "ForMaster, Ms WARE, drivers ARM, KILL." )
+        change_ARM = input( "C:/user>ARM/" )
+        if change_ARM == "DARM":
+            print( "hig, Deactivate_ARM, ARM1." )
+        else:
+            print( "" )
+            if change_ARM == "off":
+                break
+        if change_ARM == "IIIARM":
+            print( "ARM_bios, File_ARM, Programm_ARM." )
+        else:
+            print( "" )
+            if change_ARM == "File_ARM":
+                print( "DIR S CD C SS!" )
+            else:
+                print( "" )
+            if change_ARM == "ARM_bios":
+                print( "bios s s dir s ARM!" )
+    if disk == "Ms WARE Pocket":
+        print( "Instal Folder Ms WARE, developer kit Ms WARE, SIK TO KICK, SOFT." )
+        change_ARM = input( "C:/user>ARM>Ms WARE>Ms WARE Pocket/" )
+        if change_ARM == "SOFT":
+            print( "Defult MS, custompip, Ms TWO." )
+        else:
+            print( "" )
+            if change_ARM == "off":
+                break
+        if change_ARM == "Ms TWO":
+            print( "Y, N." )
+        else:
+            print( "" )
+            if change_ARM == "Y":
+                print( "you have successfully installed ms two!" )
+            else:
+                print( "" )
+            if change_ARM == "N":
+                print( "at the request of the administrator , the pocket ms two did not settle down!" )
+                disk = input( "C/:user>" )
+    if disk == "Help":
+        print( "(SOS) " )
+        change_Files = input( "C/:user>Help/" )
+        if change_Files == "SOS":
+            print( "Info..." )
+        else:
+            print( "" )
+        if change_Files == "off":
+            break
+        if change_Files == "Info":
+            print( "the system command issues three commands bios, system_32, camera_draivers, video_card_draivers please go to the bios, then one folder of boot_systems will open at once, write either ForMaster messages from ForMaster will be ARM5 NO FORMASTER, it means to write in the main menu Ms WARE Pocket, please write as the FORWARE assistant wrote, otherwise there will be nothing to work, the code was written by Ms WARE Pocket, then write Y you have successfully settled Pocket and you can write other FORWARE commands, For example in ARM, when if you have written a command and pressed enter, then write the command File_ARM will write a message in future versions, the command will be used as a command of the user, the installation of FORWARE is finished.." )
+            isk = input( "C/:user>" )
+    if disk == "Help in Russian":
+        print( "(SOS) " )
+        change_Files = input( "C/:user>Help in Russian/" )
+        if change_Files == "Info":
+            print( "Info in Russia" )
+        else:
+            print( "" )
+        if change_Files == "off":
+            break
+        if change_Files == "Info in Russia":
+            print( "системная команда выдает три команды bios, system_32, camera_draivers, video_card_draivers пожалуйста, зайдите в bios, тогда сразу откроется одна папка boot_systems, напишите либо Formaster, сообщения от FORMASTER будут ARM5 БЕЗ FORMASTER, это означает написать в главном меню Ms WARE Pocket, пожалуйста, напишите, как написал помощник по программному обеспечению, иначе работать будет нечего, код написан Ms WARE Pocket, затем напишите Y вы успешно установили Pocket, и вы можете написать другие команды для программного обеспечения, например, в ARM, когда, если вы написали команду и нажал enter, затем написал команду File_ARM напишет сообщение в будущих версиях, команда будет использоваться как команда пользователя, установка программного обеспечения завершена." )
